660db54427faef0229027383ae5f9189c58f5c7c
Sarang19 <rastogi.sarang2004@gmail.com>
2025-05-28 15:56:42 +0530

    overwrite

--- buoy_map/lib/buoy_map/device_store.ex
+++ buoy_map/lib/buoy_map/device_store.ex
@@ -2,21 +2,19 @@ defmodule BuoyMap.DeviceStore do
   use GenServer
 
   @max_trail_points 1000  # Maximum points to store in memory
-  @history_limit 1000
 
   # API
   def start_link(_), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
 
   def get_device(device_id), do: GenServer.call(__MODULE__, {:get_device, device_id})
   def get_trail(device_id), do: GenServer.call(__MODULE__, {:get_trail, device_id})
-  def get_history(device_id, points), do: GenServer.call(__MODULE__, {:get_history, device_id, points})
   def get_all_devices(), do: GenServer.call(__MODULE__, :get_all_devices)
   def set_devices(devices), do: GenServer.cast(__MODULE__, {:set_devices, devices})
   def update_device(device), do: GenServer.cast(__MODULE__, {:update_device, device})
   def update_trail(device_id, trail), do: GenServer.cast(__MODULE__, {:update_trail, device_id, trail})
 
   # Server
-  def init(_), do: {:ok, %{devices: %{}, trails: %{}, history: %{}}}
+  def init(_), do: {:ok, %{devices: %{}, trails: %{}}}
 
   def handle_call({:get_device, id}, _from, state) do
     {:reply, Map.get(state.devices, id), state}
@@ -26,61 +24,38 @@ defmodule BuoyMap.DeviceStore do
     {:reply, Map.get(state.trails, id, []), state}
   end
 
-  def handle_call({:get_history, id, points}, _from, state) do
-    device = Map.get(state.devices, id)
-    if device do
-      history = Map.get(state.history, id, [])
-      limited_history = Enum.take(history, points)
-      {:reply, limited_history, state}
-    else
-      {:reply, [], state}
-    end
-  end
-
   def handle_call(:get_all_devices, _from, state) do
     {:reply, Map.values(state.devices), state}
   end
 
   def handle_cast({:set_devices, devices}, state) do
-    now = DateTime.utc_now()
     new_devices = for d <- devices, into: %{}, do: {d.device_id, d}
     new_trails = for d <- devices, into: %{}, do: {d.device_id, [[d.lon, d.lat]]}
-    new_history = for d <- devices, into: %{} do
-      {d.device_id, [%{
-        timestamp: now,
-        avg_speed: d.avg_speed,
-        elevation: d.elevation,
-        voltage: d.voltage,
-        rssi: d.rssi,
-        snr: d.snr
-      }]}
-    end
-    {:noreply, %{state | devices: new_devices, trails: new_trails, history: new_history}}
+    {:noreply, %{state | devices: new_devices, trails: new_trails}}
   end
 
   def handle_cast({:update_device, device}, state) do
-    device_id = device.device_id
-    current_history = Map.get(state.history, device_id, [])
+    # When updating a device, also update its trail
+    current_trail = Map.get(state.trails, device.device_id, [])
+    new_point = [device.lon, device.lat]
 
-    # Create new history entry with timestamp
-    new_entry = %{
-      timestamp: DateTime.utc_now(),
-      avg_speed: device.avg_speed,
-      elevation: device.elevation,
-      voltage: device.voltage,
-      rssi: device.rssi,
-      snr: device.snr
-    }
+    # Only add point if it's different from the last one
+    updated_trail = if List.first(current_trail) != new_point do
+      [new_point | current_trail] |> Enum.take(@max_trail_points)
+    else
+      current_trail
+    end
 
-    updated_history = [new_entry | current_history] |> Enum.take(@history_limit)
+    new_state = state
+    |> put_in([:devices, device.device_id], device)
+    |> put_in([:trails, device.device_id], updated_trail)
 
-    {:noreply, %{state |
-      devices: Map.put(state.devices, device_id, device),
-      history: Map.put(state.history, device_id, updated_history)
-    }}
+    {:noreply, new_state}
   end
 
   def handle_cast({:update_trail, device_id, trail}, state) do
-    {:noreply, %{state | trails: Map.put(state.trails, device_id, trail)}}
+    # Ensure we don't exceed maximum trail points
+    updated_trail = Enum.take(trail, @max_trail_points)
+    {:noreply, %{state | trails: Map.put(state.trails, device_id, updated_trail)}}
   end
 end
