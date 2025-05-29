a861d986039bcaea8f997dc47d04335752c8256c
S1ddharthaShukla <252sid2012@gmail.com>
2025-05-29 14:58:24 +0530

    fixed authentication

--- buoy_map/lib/buoy_map/accounts/user_token.ex
+++ buoy_map/lib/buoy_map/accounts/user_token.ex
@@ -20,7 +20,7 @@ defmodule BuoyMap.Accounts.UserToken do
     field :token, :binary
     field :context, :string
     field :sent_to, :string
-    belongs_to :user, BuoyMap.Accounts.User
+    belongs_to :user, BuoyMap.Accounts.User, type: :binary_id
 
     timestamps(type: :utc_datetime, updated_at: false)
   end
